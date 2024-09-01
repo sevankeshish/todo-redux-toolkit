be6097bb79da9103261e6e95c9a8c5d41c69aa9c
Hexact task for Junior React JS <sevan.keshish0@gmail.com>
2024-09-01 16:12:04 -0700

    TodoSlice

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import AddTodoForm from "./conponents/Todos/AddTodoForm";
 import "bootstrap/dist/css/bootstrap.min.css";
+import AddTodoForm from "./conponents/Todos/AddTodoForm";
 import TodoList from "./conponents/Todos/TodoList";
+import { useSelector } from "react-redux";
 
 function App() {
+  const todos = useSelector((state) => state.todos);
+  console.log(todos);
   return (
     <div className="container pt-3">
       <h1 className="text-center">TodoApp with RTK</h1>
