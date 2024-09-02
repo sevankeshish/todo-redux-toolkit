81a9bb533dd9b1248e0971859217bb9214148daa
Hexact task for Junior React JS <sevan.keshish0@gmail.com>
2024-09-02 12:30:37 -0700

    deleteTodo and ToggleTodo

--- src/conponents/Todos/TodoItem.jsx
+++ src/conponents/Todos/TodoItem.jsx
@@ -1,12 +1,26 @@
+import { useDispatch } from "react-redux";
+import { deleteTodo, toggleTodo } from "../../features/todo/todoSlice";
+
 const TodoItem = ({ id, title, completed }) => {
+  const dispatch = useDispatch();
   return (
     <li className={`list-group-item ${completed && "list-group-item-success"}`}>
       <div className="d-flex justify-content-between">
         <span className="d-flex align-items-center gap-1">
-          <input type="checkbox" className="mr-3" checked={completed} />
+          <input
+            type="checkbox"
+            className="mr-3"
+            checked={completed}
+            onChange={(e) => dispatch(toggleTodo({ id }))}
+          />
           <span>{title}</span>
         </span>
-        <button className="btn btn-danger">Delete</button>
+        <button
+          className="btn btn-danger"
+          onClick={() => dispatch(deleteTodo({ id }))}
+        >
+          Delete
+        </button>
       </div>
     </li>
   );
